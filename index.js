2ed61a1ddbe28762411dbf0ef9da8f32494a6dbc
viansebastian <viansbromokusumo@gmail.com>
2023-10-12 13:35:54 +0700

    update index

--- index.js
+++ index.js
@@ -59,7 +59,6 @@ app.put('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if (!course) return res.status(404).send('Course not found');
 
-
     //validate, if invalid return 404
     const { error } = validateCourse(req.body); // result.error
     if(error) return res.status(400).send(error.details[0].message);
